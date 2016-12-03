76a2b20fb5a4399bc431bf4d1418d3be2d35b931
vladiko <koganvladimir@yahoo.com>
2016-12-03 23:23:58 +0200

    test deployment

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Shmira\n');
+    res.end('Hello Shmira test\n');
 }).listen(port);
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -2,5 +2,5 @@
 var port = process.env.port || 1337
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Shmira\n');
+    res.end('Hello Shmira test\n');
 }).listen(port);
