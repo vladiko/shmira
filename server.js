e7c6e2d34c388e62be76ccec7ae47d638fd084b0
vladiko <koganvladimir@yahoo.com>
2016-12-03 23:14:16 +0200

    fix word shmira

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Smira\n');
+    res.end('Hello Shmira\n');
 }).listen(port);
 //# sourceMappingURL=server.js.map
