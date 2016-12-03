c0e6b10da9ed5eba4eb9ba86c8e8457d1ea17a93
vladiko <koganvladimir@yahoo.com>
2016-12-04 00:24:08 +0200

    change gitignor

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Shmira test next dep\n');
+    res.end('Hello Shmira test next dep2\n');
 }).listen(port);
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -2,5 +2,5 @@
 var port = process.env.port || 1337
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Shmira test next dep\n');
+    res.end('Hello Shmira test next dep2\n');
 }).listen(port);
